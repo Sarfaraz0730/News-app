6da32b3d8cad2b8544aa3025ac78f8bc42f4b868
Sarfaraz0730 <sarfarazpycoder@gmail.com>
2023-12-16 17:44:37 +0530

    api fixed

--- index.js
+++ index.js
@@ -1,66 +1,60 @@
-const API_KEY = "171b35340cf7465f8fe72a5fdeac867a"
+const API_KEY = "171b35340cf7465f8fe72a5fdeac867a";
 const URL = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
-var search =""
+var search = "";
 var searchResult;
-// const everything =`https://newsapi.org/v2/everything?q=bitcoin&apiKey=171b35340cf7465f8fe72a5fdeac867a`
-
 const contentDiv = document.getElementById("content-div");
-
 
 async function fetchNews(api) {
     try {
         const response = await fetch(api);
         const data = await response.json();
-        console.log("data", data.articles);
+
+        // Clear existing content before rendering new data
+        contentDiv.innerHTML = "";
 
         data.articles.forEach((item, i) => {
-          
-            const truncatedDescription = item.description.length > 250
-            ? item.description.slice(0, 100) + '...' 
-            : item.description;
-            const titleHeading =item.title.length>100? item.title.slice(0,60) + "...":item.title
+            const truncatedDescription =
+                item.description.length > 250
+                    ? item.description.slice(0, 100) + "..."
+                    : item.description;
+            const titleHeading =
+                item.title.length > 100 ? item.title.slice(0, 60) + "..." : item.title;
             var box = document.createElement("div");
             box.className = "box";
 
             box.innerHTML = `<div class="image-div">
-            <img class="img" src=${item.urlToImage} alt="news banner">
-           </div>
+                <img class="img" src=${item.urlToImage} alt="news banner">
+            </div>
 
-
-           <div class="news-heading"><h4>${titleHeading}</h4></div>
-           <div class="discription">${truncatedDescription}</div>
-           <div class="read-more"><span id="read-text">Read full article</span> <button id="read-more-btn">  <u><span class="material-symbols-outlined">
-            <u>arrow_right_alt</u>
-            </span></u>
-         </button>
-        </div>`
-
-
-            
+            <div class="news-heading"><h4>${titleHeading}</h4></div>
+            <div class="discription">${truncatedDescription}</div>
+            <div class="read-more"><span id="read-text">Read full article</span> <button id="read-more-btn">  <u><span class="material-symbols-outlined">
+                <u>arrow_right_alt</u>
+            </span></u></button></div>`;
 
             contentDiv.append(box);
         });
-
     } catch (err) {
-        console.log("ERROR : ", err)
+        console.log("ERROR : ", err);
     }
 }
-function getInput(){
-    var inputValue = document.getElementById('input').value;
-    search +=inputValue
 
- searchResult =`https://newsapi.org/v2/everything?q=${search}&apiKey=171b35340cf7465f8fe72a5fdeac867a`;
-// fetchNews(searchResult)
-console.log("searchResult",searchResult)
-if(search ==""){
+function getInput() {
+    var inputValue = document.getElementById("input").value;
+    search = inputValue;
+
+    searchResult = `https://newsapi.org/v2/everything?q=${search}&apiKey=${API_KEY}`;
+
+    console.log("searchResult", searchResult);
+
+    if (search === "") {
+        fetchNews(URL);
+    } else {
+        fetchNews(searchResult);
+    }
+}
+
+// Initial loading of news when the page loads
+document.addEventListener("DOMContentLoaded", function () {
     fetchNews(URL);
-}else{
-    fetchNews(searchResult)
-}
-
-
-
-
-}
-
-
+});
