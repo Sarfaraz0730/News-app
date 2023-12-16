731c15c901845b0888c4be103c24b36072efd2ea
Sarfaraz0730 <sarfarazpycoder@gmail.com>
2023-12-16 14:11:11 +0530

    working on api

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 console.log("hello")
+const API_KEY ="171b35340cf7465f8fe72a5fdeac867a"
+const URL = `https://newsapi.org/v2/everything?q=india&from=2023-11-15&sortBy=publishedAt&apiKey=${API_KEY}`;
+
+const contentDiv=document.getElementById("content-div");
+
+
+async function fetchNews(api){
+    var box = document.createElement("div");
+    box.className="box";
+
+try{
+    const response = await fetch(api);
+    const data = await response.json();
+    console.log("data",data.articles)
+}catch(err){
+    console.log("ERROR : ", err)
+}
+}
+fetchNews(URL)
+
